ebbe1d70f627137f4f7fc5210828a0251dc15224
Taprik <taprik@taprik.net>
2019-03-20 14:55:14 +0100

    Update BluetoothTerminal.js

--- js/BluetoothTerminal.js
+++ js/BluetoothTerminal.js
@@ -240,7 +240,38 @@ class BluetoothTerminal {
    */
   _requestBluetoothDevice() {
     //////////////////////////////////////////////////////////
-    
+    log('Requesting Bluetooth Device...');
+  navigator.bluetooth.requestDevice({filters: [{services: [this._serviceUuid]}]})
+  .then(device => {
+    log('Connecting to GATT Server...');
+    return device.gatt.connect();
+  })
+  .then(server => {
+    log('Getting Service...');
+    return server.getPrimaryService(serviceUuid);
+  })
+  .then(service => {
+    log('Getting Characteristic...');
+    return service.getCharacteristic(characteristicUuid);
+  })
+  .then(characteristic => {
+    log('> Characteristic UUID:  ' + characteristic.uuid);
+    log('> Broadcast:            ' + characteristic.properties.broadcast);
+    log('> Read:                 ' + characteristic.properties.read);
+    log('> Write w/o response:   ' +
+      characteristic.properties.writeWithoutResponse);
+    log('> Write:                ' + characteristic.properties.write);
+    log('> Notify:               ' + characteristic.properties.notify);
+    log('> Indicate:             ' + characteristic.properties.indicate);
+    log('> Signed Write:         ' +
+      characteristic.properties.authenticatedSignedWrites);
+    log('> Queued Write:         ' + characteristic.properties.reliableWrite);
+    log('> Writable Auxiliaries: ' +
+      characteristic.properties.writableAuxiliaries);
+  })
+  .catch(error => {
+    log('Argh! ' + error);
+  });
     /////////////////////////////////////////////////////////
     this._log('Requesting bluetooth device...');
     return navigator.bluetooth.requestDevice({
